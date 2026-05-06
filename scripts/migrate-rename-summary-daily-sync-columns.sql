4b6f92b20276f3659f786459637f9c483d0603f4
Nhuong <nhuongblue@gmail.com>
2026-05-06 15:23:20 +0700

    feat: remove deprecated sync metadata migration scripts

--- scripts/migrate-rename-summary-daily-sync-columns.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE air_quality.fct_air_quality_summary_daily
-    RENAME COLUMN IF EXISTS max_raw_loaded_at TO raw_loaded_at;
-
-ALTER TABLE air_quality.fct_air_quality_summary_daily
-    RENAME COLUMN IF EXISTS latest_raw_sync_run_id TO raw_sync_run_id;
-
-ALTER TABLE air_quality.fct_air_quality_summary_daily
-    RENAME COLUMN IF EXISTS latest_raw_sync_started_at TO raw_sync_started_at;
